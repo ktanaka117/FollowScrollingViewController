7cd04b6eea0498899ce268b16fed4a52a2ea1d60
田中賢治 <key.m4096.kung.fu@gmail.com>
2016-06-13 21:18:49 +0900

    Initial Commit

---
+++ FollowScrollingViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FollowScrollingViewController
+//
+//  Created by 田中賢治 on 2016/06/13.
+//  Copyright © 2016年 田中賢治. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
